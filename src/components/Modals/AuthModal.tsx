65806a63b7558e6236031d49ce175f0b88c9f472
davidadediji <davidadediji@gmail.com>
2023-06-02 22:25:39 +0100

    chore:add close modal functionality

--- src/components/Modals/AuthModal.tsx
+++ src/components/Modals/AuthModal.tsx
@@ -1,20 +1,22 @@
-import React from 'react'
-import {IoClose} from 'react-icons/io5'
+import React, { useEffect } from 'react';
+import { IoClose } from 'react-icons/io5';
 import LogIn from './LogIn';
 import SignUp from './SignUp';
 import ResetPassword from './ResetPassword';
-import { useRecoilValue } from 'recoil';
+import { useRecoilValue, useSetRecoilState } from 'recoil';
 import { authModalState } from '@/atoms/authModalAtom';
 
-interface AuthModalProps{
-
-}
+interface AuthModalProps {}
 
 export default function AuthModal() {
-const authModal = useRecoilValue(authModalState);
-  return (
+	const authModal = useRecoilValue(authModalState);
+	const handleClick = useCloseModal();
+	return (
 		<>
-			<div className='absolute top-0 left-0 w-full h-full flex items-center justify-center bg-black bg-opacity-60'></div>
+			<div
+				className='absolute top-0 left-0 w-full h-full flex items-center justify-center bg-black bg-opacity-60'
+				onClick={handleClick}
+			></div>
 			<div className='w-full sm:w-[450px]  absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]  flex justify-center items-center'>
 				<div className='relative w-full h-full mx-auto flex items-center justify-center'>
 					<div className='bg-white rounded-lg shadow relative w-full bg-gradient-to-b from-brand-orange to-slate-900 mx-6'>
@@ -22,14 +24,41 @@ const authModal = useRecoilValue(authModalState);
 							<button
 								type='button'
 								className='bg-transparent  rounded-lg text-sm p-1.5 ml-auto inline-flex items-center hover:bg-gray-800 hover:text-white text-white'
+								onClick={handleClick}
 							>
-								<IoClose className=' h-5 w-5'/>
+								<IoClose className=' h-5 w-5' />
 							</button>
 						</div>
-                        {authModal.type === 'login'? <LogIn/> : authModal.type === 'register'? <SignUp/> :<ResetPassword/>}
+						{authModal.type === 'login' ? (
+							<LogIn />
+						) : authModal.type === 'register' ? (
+							<SignUp />
+						) : (
+							<ResetPassword />
+						)}
 					</div>
 				</div>
 			</div>
 		</>
 	);
+}
+
+function useCloseModal() {
+	const setAuthModalState = useSetRecoilState(authModalState);
+
+	const closeModal = () => {
+		setAuthModalState((prev) => ({ ...prev, isOpen: false, type:'login' }));
+	};
+
+	useEffect(() => {
+		const handleEscape = (event: KeyboardEvent) => {
+			if (event.key === 'Escape') {
+				closeModal();
+			}
+		};
+		window.addEventListener('keydown', handleEscape);
+		return () => window.removeEventListener('keydown', handleEscape);
+	}, []);
+
+	return closeModal;
 }
