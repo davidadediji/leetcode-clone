6040474b145ca7feee1930a5a9c1187cd8904d59
davidadediji <davidadediji@gmail.com>
2023-06-02 16:51:11 +0100

    create:initial files

--- src/middlewares/not-found.ts
+++
@@ -1,5 +0,0 @@
-import { NextFunction, Request, Response } from 'express';
-
-export function notFound(req: Request, res: Response, next: NextFunction) {
-	return res.status(200).json({ message: 'Route does not exist' });
-}
